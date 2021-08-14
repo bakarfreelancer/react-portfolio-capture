55dddbacc1aa3b3e1ca9cd25d274dd73b6c8b689
bakarfreelancer <bakarfreelancer96@gmail.com>
2021-08-14 14:56:58 +0500

    about section style added

---
+++ src/components/AboutSection.js
@@ -0,0 +1,60 @@
+import home1 from "../img/home1.png";
+
+import styled from "styled-components";
+const AboutSection = () => {
+  return (
+    <About>
+      <Description>
+        <Hide>
+          <h2>We work to make</h2>
+        </Hide>
+        <Hide>
+          <h2>
+            your <span>dreams</span> come
+          </h2>
+        </Hide>
+        <Hide>
+          <h2>true.</h2>
+        </Hide>
+        <p>
+          Contact us for any photography or videography ideas that you have we
+          have professionals with amaizing skills .
+        </p>
+        <button>Contact Us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="camera guy" />
+      </Image>
+    </About>
+  );
+};
+
+// Styled component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: contain;
+  }
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+export default AboutSection;
---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Questions <span>FAQ</span>
+      </h2>
+
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos totam repudiandae.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos totam repudiandae.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos totam repudiandae.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Dignissimos totam repudiandae.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default FaqSection;
